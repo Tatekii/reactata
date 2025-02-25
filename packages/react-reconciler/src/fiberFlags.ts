b4792c3c04b1814c47e42a8c386e9afb9041e3f0
Siyn Ma <mrtatekii33@gmail.com>
2025-02-25 10:27:51 +0800

    reconciler construct

---
+++ packages/react-reconciler/src/fiberFlags.ts
@@ -0,0 +1,10 @@
+/**
+ * 标识不同副作用
+ */
+export type Flags = number;
+
+export const NoFlags = 0b0000000;
+export const PerformedWork = 0b0000001;
+export const Placement = 0b0000010;
+export const Update = 0b0000100;
+export const ChildDeletion = 0b0001000;
